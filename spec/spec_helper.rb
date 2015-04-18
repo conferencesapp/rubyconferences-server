51533883c00d2376be439efde35b4a0ed9727e4e
Arun Agrawal <arunagw@gmail.com>
2015-04-18 23:20:09 +0200

    Initial load

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+if ENV.fetch("COVERAGE", false)
+  require "simplecov"
+  SimpleCov.start "rails"
+end
+
+require "webmock/rspec"
+
+# http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+
+  config.order = :random
+end
+
+WebMock.disable_net_connect!(allow_localhost: true)
