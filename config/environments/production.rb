b7848fe8793b9931cee569ee6e6c64479eef4c2a
Arun Agrawal <arunagw@gmail.com>
2015-05-09 10:45:10 +0200

    Do not use HOST

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  # config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -7,5 +7,5 @@ Mail.register_interceptor(
 Rails.application.configure do
   # ...
 
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  # config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
