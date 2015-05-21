aa05348e54e4ce0cb56bb2a904a0ffb9ba6c07e5
Arun Agrawal <arunagw@gmail.com>
2015-05-21 15:28:08 +0200

    Setup staging deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,7 @@
 set :stage, :production
 set :rails_env, :production
-set :unicorn_rack_env, :production
 
 set :deploy_to, "/home/sites/sites/#{fetch(:application)}/production"
-set :unicorn_bundle, "source $HOME/.bash_profile && bundle"
 
 role :app, %w{sites@box.rubyrockers.com}
 role :web, %w{sites@box.rubyrockers.com}
