10174d5431d908d2b7df6cd65b02fd393ef9d127
Arun Agrawal <arunagw@gmail.com>
2015-05-21 15:52:15 +0200

    Remove deploy scripts

--- config/deploy/production.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :production
-set :rails_env, :production
-
-set :deploy_to, "/home/sites/sites/#{fetch(:application)}/production"
-
-role :app, %w{sites@box.rubyrockers.com}
-role :web, %w{sites@box.rubyrockers.com}
-role :db,  %w{sites@box.rubyrockers.com}
--- config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-set :stage, :staging
-set :rails_env, :staging
-
-set :deploy_to, "/home/sites/sites/#{fetch(:application)}/staging"
-
-role :app, %w{sites@box.rubyrockers.com}
-role :web, %w{sites@box.rubyrockers.com}
-role :db,  %w{sites@box.rubyrockers.com}
