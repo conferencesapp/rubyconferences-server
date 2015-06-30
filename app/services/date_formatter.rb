842b3f54afb3385f30afa39fa3442121f54192f1
Arun Agrawal <arunagw@gmail.com>
2015-06-30 15:41:29 +0200

    Refactor `DateFormatter` to use delegate

--- app/services/date_formatter.rb
+++ app/services/date_formatter.rb
@@ -1,4 +1,7 @@
 class DateFormatter
+  delegate :day, :month, :year, to: :start_date_text, prefix: true
+  delegate :day, :month, :year, to: :end_date_text, prefix: true
+
   def initialize(start_date, end_date)
     @start_date = start_date
     @end_date = end_date
@@ -17,20 +20,28 @@ class DateFormatter
   attr_reader :start_date, :end_date
 
   def simple
-    "#{start_date_text}, #{start_date_year_text}"
+    "#{combined_start_date_text}, #{start_date_text_year}"
   end
 
   def formatted
-    if start_date_year_text == end_date_year_text
-      if start_date_month_text == end_date_month_text
-        "#{start_date_text}-#{end_date_date_text}, #{end_date_year_text}"
-      else
-        "#{start_date_text} - #{end_date_month_text}"\
-          " #{end_date_date_text}, #{end_date_year_text}"
-      end
+    if start_date_text_year == end_date_text_year
+      format_with_same_year
     else
-      "#{start_date_text}, #{start_date_year_text} -"\
-        " #{end_date_month_text} #{end_date_date_text}, #{end_date_year_text}"
+      format_with_differnet_year
+    end
+  end
+
+  def format_with_differnet_year
+    "#{combined_start_date_text}, #{start_date_text_year} -"\
+      " #{end_date_text_month} #{end_date_text_day}, #{end_date_text_year}"
+  end
+
+  def format_with_same_year
+    if start_date_text_month == end_date_text_month
+      "#{combined_start_date_text}-#{end_date_text_day}, #{end_date_text_year}"
+    else
+      "#{combined_start_date_text} - #{end_date_text_month}"\
+        " #{end_date_text_day}, #{end_date_text_year}"
     end
   end
 
@@ -38,43 +49,37 @@ class DateFormatter
     start_date == end_date
   end
 
+  def combined_start_date_text
+    "#{start_date_text_month} #{start_date_text_day}"
+  end
+
   def start_date_text
-    @start_date_date_text ||= "#{start_date_month_text} #{start_date_date_text}"
+    @start_date_text ||= DateText.new(start_date)
   end
 
-  def start_date_month_text
-    @start_date_month_text ||= month_text(start_date)
+  def end_date_text
+    @end_date_text ||= DateText.new(end_date)
   end
 
-  def start_date_date_text
-    @start_date_date_text ||= date_text(start_date)
-  end
+  class DateText
+    def initialize(date)
+      @date = date
+    end
 
-  def start_date_year_text
-    @start_date_year_text ||= year_text(start_date)
-  end
+    def day
+      date.strftime("%d")
+    end
 
-  def end_date_month_text
-    @end_date_month_text ||= month_text(end_date)
-  end
+    def month
+      date.strftime("%B")
+    end
 
-  def end_date_date_text
-    @end_date_date_text ||= date_text(end_date)
-  end
+    def year
+      date.year
+    end
 
-  def end_date_year_text
-    @end_date_year_text ||= year_text(end_date)
-  end
+    private
 
-  def date_text(date)
-    date.strftime("%d")
-  end
-
-  def month_text(date)
-    date.strftime("%B")
-  end
-
-  def year_text(date)
-    date.strftime("%Y")
+    attr_reader :date
   end
 end
