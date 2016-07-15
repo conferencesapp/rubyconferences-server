fd85bfcfe32ed978f564165216bfb93d5049e7b4
Rashmi Yadav <rays.rashmi@gmail.com>
2016-07-15 22:48:17 +0530

    Create a new page for showing pending announcements

--- app/models/conference_announcement.rb
+++ app/models/conference_announcement.rb
@@ -2,4 +2,8 @@ class ConferenceAnnouncement < ActiveRecord::Base
   belongs_to :conference
 
   validates :conference_id, :body, presence: true
+
+  def self.pending
+    where(sent: false)
+  end
 end
